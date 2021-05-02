7f4d614f42dd7d86f4d0d77acf85ab22861906fb
Rafael S. Alves Nobre <rsan_92@outlook.com>
2021-05-02 19:10:49 -0300

    criando a classe Posicao

---
+++ JogoXadezCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JogoXadezCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
